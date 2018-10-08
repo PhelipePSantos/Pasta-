fe0f5d21bb182cef5ee1ed9f447a6f1f08837ada
Hellen <hellencarolinecbj@hotmail.com>
2018-10-08 11:21:59 -0300

    Print e scan (escreva leia)

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -22,6 +22,57 @@ bool d; // booleano (verdadeiro/falso) (true/false)
 
 int main()
 {
+	//variavel local
+	//(visivel somente nesta função, a partir dessa linha)
+	int e;
+
+
+	printf("Hello world! MITO 17\n");
+	printf("Sao Caetano Gigante azulao do universo\n");
+	printf("Manchester\t\tUnheineken\n"),// \t Faz o texto ter o espaço de um tab
+
+	printf("\n");
+	
+	// como imprimir valores
+	printf("%i", 99);
+	printf("%f\n", 100.01);
+
+	//texto (string) e valores juntos
+	printf("Aqui vai o valor %i, total da soma. \n", 98 + 5);
+
+
+
+	printf("%i + %i = %i\n", 500, 600, 500 + 600);
+
+	printf("Por algum acaso esqueci o valor daqui: %i\n");
+
+
+	a = 150;
+	e = 275;
+
+	int f = a + e;
+
+	printf("O resultado da soma eh: %i\n", f);
+
+	//Leitura SCANF / SCANF_S
+	scanf_s("%i", &f),
+
+	printf("\nVoce digitou %i\n", f);
+
+	printf("Digite um valor real(float");
+	scanf_s("%f", &b);
+
+	printf("\nVoce digitou %f\n", b);
+
+
+
+
+
+
+
+
+	system("pause");
     return 0;
 }
+
 
