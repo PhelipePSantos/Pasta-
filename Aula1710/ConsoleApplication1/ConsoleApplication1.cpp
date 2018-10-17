676895248d93afd4334a6fc204a1ad8665458240
1821681 <1821681@DOSO121796>
2018-10-17 10:37:11 -0300

    Variaveis

---
+++ Aula1710/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+//Condicionais if=se(Aqui vai a condição) {=sentão     }=fimse 
+//== igual
+//!= diferente
+//< menor
+//> maior
+//<= menor ou igual
+//>= maior ou igual
+
+int main()
+{
+    return 0;
+}
+
