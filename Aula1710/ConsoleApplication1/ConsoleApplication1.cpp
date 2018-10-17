b5420f73c42235148c3229baa2daa73c91e5e795
1821681 <1821681@DOSO121796>
2018-10-17 10:51:18 -0300

    Variaveis

--- Aula1710/ConsoleApplication1/ConsoleApplication1.cpp
+++ Aula1710/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,9 +10,34 @@
 //> maior
 //<= menor ou igual
 //>= maior ou igual
+//&& "E"
+//|| "OU"
+//
+
 
 int main()
-{
+{	
+	int a = 5;
+	if(true)
+	{
+		printf("Sempre vai imprimir essa frase\n");
+	}
+
+	if (false)
+	{
+		printf("Essa frase nunca sera impressa");
+	}
+
+	if (a == 3)
+	{
+		printf("A eh igual a 3");
+	}
+
+	if (a != 3)
+	{
+		printf("A eh diferente de 3");
+	}
+
     return 0;
 }
 
