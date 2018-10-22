2bf4eca06422aac74c8854da1b72d4085f58bbb4
1821681 <1821681@DOSO121796>
2018-10-22 09:16:17 -0300

    Maior numero professor

---
+++ Aula2210/Maior Numero Professor/Maior Numero Professor.cpp
@@ -0,0 +1,42 @@
+// Maior Numero Professor.cpp : Defines the entry point for the console application.
+//
+
+// Aula2210.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+int maior(int a, int b, int c, int d) {
+	int mai = a;
+	if (mai < b) {
+		mai = b;
+	}
+
+	if (mai < c) {
+		mai = c;
+	}
+	if (mai < d) {
+		mai = d;
+	}
+	return mai;
+}
+
+
+int main()
+{
+
+	int a, b, c, d;
+	printf_s("Digite o primeiro valor\n");
+	scanf_s("%i", &a);
+	printf_s("Digite o segundo valor\n");
+	scanf_s("%i", &b);
+	printf_s("Digite o terceiro valor\n");
+	scanf_s("%i", &c);
+	printf_s("Digite o quarto valor\n");
+	scanf_s("%i", &d);
+	maior(a, b, c, d);
+	printf_s("O maior numero eh : %i\n ", maior(a, b, c, d));
+	system("pause");
+	return 0;
+}
