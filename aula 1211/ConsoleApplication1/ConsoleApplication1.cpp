2be0004f317641293eee749ac35cd5a2bb6affa3
1821681 <1821681@DOSO121796>
2018-11-12 10:58:19 -0200

    Recursividade

---
+++ aula 1211/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+// Recursividade
+
+#include "stdafx.h"
+#include <stdlib.h>
+int Numero()
+{
+	int num;
+	printf("Digite um numero inteiro\n");
+	scanf_s("%i", &num);
+	return num;
+}
+int soma(int n)
+{
+	if (n == 0)
+		return 0;
+	else
+		return n + soma(n - 1);
+}
+
+int main()
+{
+	int num = Numero();
+	int result = soma(num);
+	printf("Resultado: %i", result);
+	system("pause");
+    return 0;
+}
+
