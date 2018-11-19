af7e9d56f8858624e4e6be76555e40d9fc8a97e1
1821681 <1821681@DOSO121796>
2018-11-19 11:15:11 -0200

    Exercicio 21

---
+++ Aula3110/Exercicio 21/Exercicio 21.cpp
@@ -0,0 +1,35 @@
+// Exercicio 21.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+int Numero()
+{
+	int numero;
+	printf("Favor digitar um numero\n");
+	scanf_s("%i", &numero);
+	return numero;
+}
+
+void somapar()
+{
+	int n, i, par, soma=0;
+	n = Numero();
+	for (i = 0; i >= n; i++)
+	{
+		par = i % 2;
+		if(par=0)
+		{
+			soma = soma + i;
+		}
+	}
+	printf("O resultado da soma eh: %i", soma);
+}
+
+int main()
+{
+	somapar();
+	system("pause");
+    return 0;
+}
+
